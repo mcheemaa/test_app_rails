a9a1043636d99cdc68e5fc83ab462bfd66efca1a
Muhammad Ahmed Cheema <cheemawrites@gmail.com>
2021-06-22 15:00:16 -0400

    Display Articles

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'pages#home' # you dont have to write the whole name rails will do 
   get 'about' , to: 'pages#about'
 
+  resources :articles, only: [:show]
+
+
 
   
 end
