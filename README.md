61ad80a57f9280dc09761619a2158b11d818498a
Muhammad Ahmed Cheema <cheemawrites@gmail.com>
2021-06-23 10:00:33 -0400

    Added Delete & Show Features

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Checking Git Visual Studio
 First Test Repo For Ruby 
 
 Home & About page added.
+
+REST - Representational State Transfer - mapping HTTP Verbs ( GET, POST, PUT / PATCH & DELETE)
+
+resources provides REST-ful routes to Rails resources 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'pages#home' # you dont have to write the whole name rails will do 
   get 'about' , to: 'pages#about'
 
-  resources :articles , only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles 
 
 
 
