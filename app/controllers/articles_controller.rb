c5af880f6fd43a8d1c1ad11615a542d19db10296
Muhammad Ahmed Cheema <cheemawrites@gmail.com>
2021-06-23 10:30:45 -0400

    Code Clean Up - HTML Partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def show
-        
-        @article = Article.find(params[:id]) 
     end
 
     def index
@@ -10,7 +10,6 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id]) 
     end
 
     def new
@@ -28,7 +27,6 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(params.require(:article).permit(:title, :description))
             flash[:notice] = "Article was updated successfully."
             redirect_to @article
@@ -39,12 +37,15 @@ class ArticlesController < ApplicationController
 
     def destroy
     
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to articles_path
 
     end
 
+    private
 
+    def set_article
+        @article = Article.find(params[:id])
+    end
 
 end
